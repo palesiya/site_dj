373dbb41f70c3427c842cb3c59fd9eadb61704e9
Alesya Pavluchenko <anrx16@gmail.com>
2023-12-21 18:19:19 +0300

    make class in contact

--- contact/views.py
+++ contact/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import FormView
 from contact.forms import ContactForm
 from django.http import HttpResponseRedirect
 
 
-def contact(request):
-    cform = ContactForm()
-    if request.method == 'POST':
-        cform = ContactForm(request.POST)
-        if cform.is_valid():
-            cform.save()
-            return HttpResponseRedirect(request.headers.get("Referer" or "/"))
-    return render(request, 'contact.html', {"form": cform})
+class ContactFormView(FormView):
+    form_class = ContactForm
+    template_name = 'contact.html'
+    success_url = "/contact/"
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
